b0b527835efc342a7930529c6a332c700a62a14d
Leandro Marcelino <leandrodam93@gmail.com>
2022-10-19 16:10:15 -0300

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,11 @@ class MyApp extends StatelessWidget {
         body: ListView(
           children:
           [
-            Players('Arjen Robben', 'Altura: 180cm', 'Peso: 80kg', 'Pé esq', 'Posição: Ponta direita'),
-            Players('Robin Van Persie', 'Altura: 183cm', 'Peso: 71kg', 'Pé esq', 'Posição: Atacante'),
-            Players('Patrick Kluivert', 'Altura: 189cm', 'Peso: 82kg', 'Pé dir', 'Posição: Atacante'),
-            Players('Memphis Depay', 'Altura: 177cm', 'Peso: 78kg', 'Pé dir', 'Posição: Ponta esquerda'),
-            Players('Matthijs de Ligt', 'Altura: 189cm', 'Peso: 89kg', 'Pé dir', 'Zagueiro'),
+            Players('Arjen Robben', 'Altura: 180cm', 'Peso: 80kg', 'Pé esq', 'Posição: Ponta dir.', 'https://www.lance.com.br/files/article_main/uploads/2018/12/02/5c0417bd2a201.jpeg', 5),
+            Players('Robin Van Persie', 'Altura: 183cm', 'Peso: 71kg', 'Pé esq', 'Posição: Atacante', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Loco-Fener_%2810%29.jpg/265px-Loco-Fener_%2810%29.jpg', 5),
+            Players('Patrick Kluivert', 'Altura: 189cm', 'Peso: 82kg', 'Pé dir', 'Posição: Atacante', 'https://www.ogol.com.br/img/jogadores/95/769195_ori__20210403203542_patrick_kluivert.png', 4),
+            Players('Memphis Depay', 'Altura: 177cm', 'Peso: 78kg', 'Pé dir', 'Posição: Ponta esq.', 'https://img.a.transfermarkt.technology/portrait/header/167850-1597927069.jpg?lm=1', 3),
+            Players('Matthijs de Ligt', 'Altura: 189cm', 'Peso: 89kg', 'Pé dir', 'Posição: Zagueiro', 'https://images2.minutemediacdn.com/image/upload/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/shape%2Fcover%2Fsport%2FEURO-2020-group-CThe-Netherlands-v-Austria-ead1af2215b17e061d181ffd6a6cb8c0.jpg', 4),
           ],
         ),
       ),
@@ -41,9 +41,11 @@ class Players extends StatelessWidget {
   final String playerweight;
   final String playerfoot;
   final String playerposition;
+  final String photo;
+  final int ability;
 
   const Players (this.nameofplayer, this.playerheight, this. playerweight, this.playerfoot,
-      this.playerposition,
+      this.playerposition, this.photo, this.ability,
       {Key? key}) : super(key: key);
 
   @override
@@ -62,8 +64,23 @@ class Players extends StatelessWidget {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children:[
-                        Container(child: Text(playerposition, style: TextStyle(fontSize: 16,))),
-                        Container(child: Text('Habilidade')),
+                        Container(width: 165, child: Text(playerposition, style: TextStyle(fontSize: 16,))),
+                        Container(
+                            width: 70, child: Text('Habilidade', style: TextStyle(fontSize: 14),)),
+                        Container(
+                          width: 130,
+                          child:
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                              Icon(Icons.star, color: (ability >=1) ? Colors.black : Colors.white),
+                              Icon(Icons.star, color: (ability >=2) ? Colors.black : Colors.white),
+                              Icon(Icons.star, color: (ability >=3) ? Colors.black : Colors.white),
+                              Icon(Icons.star, color: (ability >=4) ? Colors.black : Colors.white),
+                              Icon(Icons.star, color: (ability >=5) ? Colors.black : Colors.white),
+                              ]
+                            )
+                        ),
                         ]
                     ),
                   ),
@@ -77,7 +94,15 @@ class Players extends StatelessWidget {
                       child: Row (
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Container(color: Colors.black54, width: 72, height: 100,),
+                          Container(
+                                decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4), color: Colors.black),
+                            width: 72, height: 100,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(4),
+                              child: Image.network(photo, fit: BoxFit.cover),
+                          ),
+                        ),
                           Container(
                             width: 100,
                               child: Text(
